1f020aa92dbe50d9f740d27e6e4d3ad0e732712f
LynnDarinKim <w0421043@nscc.ca>
2019-01-19 13:52:37 -0400

    assignment1b - question 3 completed

--- Assignment/Assignment1B/Question3/main.js
+++ Assignment/Assignment1B/Question3/main.js
@@ -1,3 +1,14 @@
+/*
+    Author: Darin Kim
+    Date: Jan. 21, 2019
+    Description: Write a JavaScript program to calculate the number of weeks, days, hours, minutes and seconds left until midnight on your birthday.
+
+•	The script does not have to prompt for your birthdate. Simply assign it to a variable and start from there.
+o	Ex: var myNextBirthday = …your code here
+•	Expected sample output (console.log()):
+o	There are 35 weeks, 3 days, 13 hours, 25 minutes, and 12 seconds until my next birthday!
+
+*/
 
 function calculateDuration()
 {
@@ -23,6 +34,4 @@ function calculateDuration()
 
 var durationResult = calculateDuration();
 
-console.log(durationResult.weeks)
-
-// console.log(`There are ${} weeks, 3 days, 13 hours, 25 minutes, and 12 seconds until my next birthday!`)
+console.log(`There are ${durationResult.weeks} weeks, ${durationResult.days} days, ${durationResult.hours} hours, ${durationResult.minutes} minutes, and ${durationResult.seconds} seconds until my next birthday!`)
