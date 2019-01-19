8dbfffb8cd48f290763d2ce7b396de8c8ae01812
LynnDarinKim <w0421043@nscc.ca>
2019-01-19 12:35:50 -0400

    backup

--- Assigment/Assignment1B/main.js
+++ Assigment/Assignment1B/main.js
@@ -3,32 +3,39 @@ var readlineSync = require('readline-sync');
 
 /*
     Author: Darin Kim
-    Date: Sept. 28, 2018
-    Description: 
+    Date: Jan. 21, 2019
+    Description: Assignment 1B Question
+
+Write a function in JavaScript that will receive a string as a parameter and then perform the following:
+
+•	You don’t have to prompt for a string. Simply assign a string to a variable in your code as your starting point to use as an argument for your function.
+•	If the first and last characters of the string are the same (ignoring case), the function will return the string in reverse order. Otherwise, the function will return the string with the first and last letters removed.
+•	Example: “Triscuit” returns “tiucsirT” but “Cracker” returns “racke”.
 
 */
 
 function main()
 {
     // Input and variables
-    var pounds = parseFloat(readlineSync.question("How many pounds of oranges do you want? "));
-    var cost_per_pound = 1.99;
-    var flat_shipping_cost = 7.50;
-    var shipping_discount = 1.50;
-    var user_location = readlineSync.question("Where are you from? ");
+    var string = "Triscuit";
+    var firstLetter = "";
+    var lastLetter = "";
+    var middleLetter = "";
 
     // Processing
-    var final_cost = pounds * cost_per_pound + flat_shipping_cost;
-    var discounted_shipping_cost = flat_shipping_cost - shipping_discount;
 
-    if (pounds >= 100 || user_location.toUpperCase() == "HALIFAX")
+    middleLetter = string.substring(1, str.length-1);
+
+    if(string.substring(0,1) === string.substring(string.length - 1, string.length))
     {
-        final_cost = pounds * cost_per_pound + discounted_shipping_cost;
+        firstLetter = string.substring(string.length - 1, string.length);
+        lastLetter = string.substring(0,1);
     }
 
-    // Output
-    console.log(`Your final cost is: $${final_cost.toFixed(2)}`)
 
+    // Output
+
+    console.log(firstLetter + middleLetter + lastLetter)
 }
 	
 if (require.main === module)
