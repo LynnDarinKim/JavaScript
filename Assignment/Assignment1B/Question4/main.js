ab4208441035a890fabb0be96ff115806fd3f2d3
LynnDarinKim <w0421043@nscc.ca>
2019-01-19 14:26:05 -0400

    assignment1b - question 4 backup

--- Assignment/Assignment1B/Question4/main.js
+++ Assignment/Assignment1B/Question4/main.js
@@ -1,37 +1,30 @@
-"use strict";
-var readlineSync = require('readline-sync');
-
 /*
     Author: Darin Kim
-    Date: Sept. 28, 2018
+    Date: Jan. 21, 2019
     Description: 
-
+    Write a JavaScript program to iterate through an array of ten(10) positive randomly generated numbers. Each number will then be checked to see if it’s a primary number.
+•	Sample Expected output (console.log()).
+23-yes, 15-no, 22-no, 124-no, 11-yes, 9-no, 2-yes, 13-yes, 5-yes, 1-no
 */
 
-function main()
+function checkPrimaryNumber()
 {
     // Input and variables
-    var pounds = parseFloat(readlineSync.question("How many pounds of oranges do you want? "));
-    var cost_per_pound = 1.99;
-    var flat_shipping_cost = 7.50;
-    var shipping_discount = 1.50;
-    var user_location = readlineSync.question("Where are you from? ");
+    var number1 = 23;
+    var number2 = 124;
 
     // Processing
-    var final_cost = pounds * cost_per_pound + flat_shipping_cost;
-    var discounted_shipping_cost = flat_shipping_cost - shipping_discount;
-
-    if (pounds >= 100 || user_location.toUpperCase() == "HALIFAX")
+    for(var i = 2; i < number1; i++)
     {
-        final_cost = pounds * cost_per_pound + discounted_shipping_cost;
+        if(number1 % i === 0)
+        {
+            return false;
+        }
+        return number1 !==1 && number1 !==0;
     }
 
-    // Output
-    console.log(`Your final cost is: $${final_cost.toFixed(2)}`)
+    // Return
 
 }
-	
-if (require.main === module)
-{
-    main();
-}
+
+// Output
