85ae6d8faf6eea0cc4e76bf40ff27d800eecebd8
LynnDarinKim <w0421043@nscc.ca>
2019-01-20 22:28:19 -0400

    didn't give up yet

--- Assignment/Assignment1B/Question2/main.js
+++ Assignment/Assignment1B/Question2/main.js
@@ -12,10 +12,17 @@ console.log("-------------")
 
 
     // Input and variables
-    // var numArray = [1, 9, 5, 6, 7];
-    // var numArray = [1, 2, 3, 6, 9, 34, 2, 6];
-    // var numArray = [3, 2, 7, 5, 6, 7, 3, 8, 9, 10, 23, 2, 1, 2, 3];
-    var numArray = [100, 101, 102, 3, 4, 5, 6, 9] ;
+    // var numArray = [1, 9, 5, 9, 7];
+    //return zero
+    
+    // var numArray = [1, 2, 3, 6, 9, 34, 2, 6]; 
+    // return 6 (1+2+3)
+
+    // var numArray = [3, 2, 7, 5, 6, 7, 3, 8, 9, 10, 23, 2, 1, 2, 3]; 
+    // return 27 (8+9+10)
+    
+    var numArray = [100, 101, 102, 3, 4, 5, 6, 9] ; 
+    // return 18 (3+4+5+6)
 
 
     // Processing
@@ -31,7 +38,7 @@ console.log("-------------")
         {
             result = true;
             counter++;
-            console.log(counter)
+            // console.log(counter)
 
             seqArray.push(counter);
             
@@ -39,76 +46,72 @@ console.log("-------------")
         else
         {
             result = false;
-            console.log("0")
+            // console.log("0")
             seqArray.push(0);
         }
 
     }
     
-    console.log("")
-    console.log(numArray)
-console.log(seqArray)
+//     console.log("")
+//     console.log(numArray)
+// console.log(seqArray)
 
 var numseqArray = [];
 
 numseqArray.push(numArray);
 numseqArray.push(seqArray);
 
-console.log(numseqArray);
+// console.log(numseqArray);
 
 
+var longSum = 0;
+var shortSum = 0;
 
-var diff = 1;
+var longSumArray = []
+var shortSumArray = []
 
-for(var c = 0; c < seqArray.length; c++)
+var sumArray = [];
+
+for(var a = 0; a < numseqArray.length; a++)
 {
-    if(seqArray[c+1] - seqArray[c] == 1 || seqArray[c] - seqArray[c+1] == -1)
+    for(var b = 0; b < numseqArray[a].length; b++)
     {
-        seqArray[c+1] = seqArray[c];
-        seqArray[c+2] = seqArray[c]; 
-        seqArray[c+3] = seqArray[c];
-    }
 
-    else
-    {
-        seqArray[c+1] = seqArray[c];
-        seqArray[c+2] = seqArray[c];
-    }
+        // if(numseqArray[1][b+1] - numseqArray[1][b] < 1)
+        // {
+        //     console.log("zero")
+        //     break;
+        // }
 
+        if(numseqArray[1][b+1] - numseqArray[1][b] == 1)
+        {
+            if(numseqArray[1][b] + numseqArray[1][b+1] > 2)
+            {
+                longSum =numseqArray[0][b] + numseqArray[0][b+1] + numseqArray[0][b+2] + numseqArray[0][b+3]
+
+                break;
+            }
+        }
+
+        if(numseqArray[1][b] !== 0 )
+        {
+            shortSum = numseqArray[0][b] + numseqArray[0][b+1]+ numseqArray[0][b+2];
+            shortSumArray.push(shortSum)
+
+            var ShortComparedResult = 0;
+            for(var i = 0; i < shortSumArray.length; i++)
+            {
+                if(shortSumArray[i] >= ShortComparedResult)
+                {
+                    ShortComparedResult = shortSumArray[i];
+                }
+            }
+        }
+        
+    }
 }
-
-console.log(seqArray)
-console.log("--work Debugger")
-
-// var sum = 0;
-
-// var sumArray = [];
-
-// for(var a = 0; a < numseqArray.length; a++)
-// {
-//     for(var b = 0; b < numseqArray[a].length; b++)
-//     {
-
-//         if(numseqArray[1][b] !== 0 )
-//         {
-            
-//             sum = numseqArray[0][b] + numseqArray[0][b+1]+ numseqArray[0][b+2];
-            
-//         }
-
-//         if(numseqArray[1][b+1] - numseqArray[1][b] == 1)
-//         {
-//             sum =numseqArray[0][b] + numseqArray[0][b+1] + numseqArray[0][b+2] + numseqArray[0][b+3]
-//         }
-
-
-
-//         sumArray.push(sum)
-//     }
-// }
-
-// console.log(sum)
-// console.log(sumArray)
+console.log(longSum)
+console.log(ShortComparedResult)
 
     // Output
 
