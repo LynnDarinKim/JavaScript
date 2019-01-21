ab841b9ed3826853b6bd56c46d9e40b9b772d92d
LynnDarinKim <w0421043@nscc.ca>
2019-01-20 23:34:19 -0400

    assignment 1b - question2 - completed.

--- Assignment/Assignment1B/Question2/main.js
+++ Assignment/Assignment1B/Question2/main.js
@@ -4,12 +4,110 @@
     Description: Write a function in JavaScript that will return the sum of the longest streak of consecutive numbers within an array. 
 •	If there are no consecutive numbers in the array, the function will return zero.
 •	If there are multiple instances of the same number of consecutive numbers in the array, the function will return the largest sum calculated between all instances.
-
 */
 
-console.log("-------------")
 
+function sumConsecutiveNumbers(numArray) 
+{    
     // Input and variables
+
+    var seqArray = [];
+
+    var longSum = 0;
+    var shortSum = 0;
+
+    var shortSumArray = []
+    var ShortComparedResult = 0;
+
+    var max = 0;
+
+    var counter = 0;
+
+
+    // Processing
+
+    // go over the number array and find if consecutive numbers are there
+    for (var i = 0; i < numArray.length; i++)
+    {
+        // the difference should be 1
+        if(numArray[i]+1 === numArray[i+1] && numArray[i+1]+1 === numArray[i+2])
+        {
+            // count from the start number and push it in the sequence array. will use this to compare if the number is consecutive or not.  
+            counter++;
+            seqArray.push(counter);
+        }
+
+        else
+        {
+            // if the number is not a starting number of consecutive numbers, push zero so that the sequence array can have same index
+            seqArray.push(0);
+        }
+    }
+
+    // go over the array and find it the consecutive number is more than 3
+    for(var a = 0; a < numArray.length; a++)
+    {
+        // if the difference between elements in sequence array is 1 and
+        if(seqArray[a+1] - seqArray[a] === 1)
+        {
+            // if the sum of consecutive sequence element is more than 2, 
+            // it means at least 4 numbers are consecutive 
+
+            /*example 
+            numArray = [3, 2, 7, 5, 6, 7, 3, 8, 9, 10, 23, 2, 1, 2, 3];
+            seqArray = [0, 0, 0, 1, 0, 0, 0, 2, 0, 0,   0,  0, 3, 0, 0];
+                                 1, *, *     2, *, *           3, *, *
+
+            numArray [100, 101, 102, 3, 4, 5, 6, 9];
+            seqArray [  1,   0,   0, 2, 3, 0, 0, 0];
+                        1,   *,   *, 2, 3, *, *,     => when numbers in seqArray is consecutive, start from the number and 3 numbers should be added. 
+            */
+            if(seqArray[a] + seqArray[a+1] > 2)
+            {
+                longSum =numArray[a] + numArray[a+1] + numArray[a+2] + numArray[a+3]
+
+                    break;
+            }
+        }
+
+        // seperated numbers in sequence array means 3 consecutive numbers are there. 
+        else if(seqArray[a] !== 0)
+        {
+            shortSum = numArray[a] + numArray[a+1]+ numArray[a+2];
+            shortSumArray.push(shortSum)
+
+                // if there's no longest streak, compare between numbers
+                for(var i = 0; i < shortSumArray.length; i++)
+                {
+                    if(shortSumArray[i] >= ShortComparedResult)
+                    {
+                        ShortComparedResult = shortSumArray[i];
+                    }
+                }
+        }
+    }
+
+    // get the maximum number in the sequence array. 
+    max = Math.max.apply(null, seqArray);
+
+    // if the maximum number is equal to 0, it means no sequence number. so return 0
+    if(max === 0)
+    {
+        return "0";
+    }
+
+    else if(longSum > 0 )
+    {
+        return longSum;
+    }
+
+    else if (longSum === 0 && ShortComparedResult > 0)
+    {
+        return ShortComparedResult;
+    }
+}
+
+    // Example Output
 
     // var numArray = [1, 9, 5, 9, 7];
     //return zero
@@ -20,88 +118,7 @@ console.log("-------------")
     // var numArray = [3, 2, 7, 5, 6, 7, 3, 8, 9, 10, 23, 2, 1, 2, 3]; 
     // return 27 (8+9+10)
     
-    // var numArray = [100, 101, 102, 3, 4, 5, 6, 9] ; 
+    var numArray = [100, 101, 102, 3, 4, 5, 6, 9] ; 
     // return 18 (3+4+5+6)
 
-
-
-    // Processing
-
-    var counter = 0;
-
-    var seqArray = [];
-    
-    for (var i = 0; i < numArray.length; i++)
-    {
-        if(numArray[i]+1 == numArray[i+1] && numArray[i+1]+1 == numArray[i+2])
-        {
-            counter++;
-            // console.log(counter)
-
-            seqArray.push(counter);
-            
-        }
-        else
-        {
-            // console.log("0")
-            seqArray.push(0);
-        }
-
-    }
-
-var longSum = 0;
-var shortSum = 0;
-
-var longSumArray = []
-var shortSumArray = []
-
-var sumArray = [];
-
-
-for(var a = 0; a < numArray.length; a++)
-{
-    if(seqArray[a+1] - seqArray[a] == 1)
-    {
-        if(seqArray[a] + seqArray[a+1] > 2)
-        {
-            longSum =numArray[a] + numArray[a+1] + numArray[a+2] + numArray[a+3]
-
-                break;
-        }
-    }
-
-    else if(seqArray[a] !==0)
-    {
-        shortSum = numArray[a] + numArray[a+1]+ numArray[a+2];
-        shortSumArray.push(shortSum)
-
-        var ShortComparedResult = 0;
-            for(var i = 0; i < shortSumArray.length; i++)
-            {
-                if(shortSumArray[i] >= ShortComparedResult)
-                {
-                    ShortComparedResult = shortSumArray[i];
-                }
-            }
-    }
-
-}
-
-// console.log(longSum)
-// console.log(ShortComparedResult)
-
-
-if(longSum > 0 )
-{
-    console.log(longSum)
-}
-
-else if (longSum === 0 && ShortComparedResult > 0)
-{
-    console.log(ShortComparedResult)
-}
-
-
-    // Output
-
-
+console.log(sumConsecutiveNumbers(numArray));
