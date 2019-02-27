3c1ab6ac4e1ff532284fef308ce0cd61d91abfc4
DARIN KIM <darinkim@DARINui-MacBook-Pro.local>
2019-02-27 05:09:58 -0400

    ㅠ_ㅠ 모르겟ㅅ어여ㅛ..

--- Assignment/Assignment3C/Assignment3C-Map/main.js
+++ Assignment/Assignment3C/Assignment3C-Map/main.js
@@ -9,55 +9,84 @@
 
     L.marker([42, -80]).addTo(map)
         .bindPopup('This is a sample popup. You can put any html structure in this including extra flight data. You can also swap this icon out for a custom icon. Some png files have been provided for you to use if you wish.')
-        .openPopup();
+        .on(onclick);
 
 
-        //test
+
+
+        // //test
       
-        var geojsonFeature2 = {"type": "Feature","geometry": {"type": "Point","coordinates": [-104.99404, 50]},"properties": {"name": "null island"}}
+        // var geojsonFeature2 = {"type": "Feature","geometry": {"type": "Point","coordinates": [-104.99404, 50]},"properties": {"name": "null island"}}
 
-        L.geoJSON(geojsonFeature2).addTo(map);
-        //test
-
-        // 5, 6, 2nd 
-        // `{"type": "Feature","geometry": {"type": "Point","coordinates": [${x[5]}, ${x[6]}]},"properties": {"name": ${x[2]}}}`
+        // L.geoJSON(geojsonFeature2).addTo(map);
+        // //test
 
         var getLongitudeLatitudeName = function(jsonInput) {
             return jsonInput.states.map(function(x){
-                // return {"type": "Feature","geometry": {"type": "Point","coordinates": [parseFloat(`${(x[5])}`), parseFloat(`${x[6]}`)]},"properties": {"name": `${x[2]}`}};
-
-                    if(x[5] !== null && x[6] !== null && x[2] !== null) {
-                        return {"type": "Feature","geometry": {"type": "Point","coordinates": [parseFloat(`${(x[5])}`), parseFloat(`${x[6]}`)]},"properties": {"name": `${x[2]}`}};   
-                    } else {return 0}
-
-                
+                if(x[5] !== null && x[6] !== null && x[2] !== null) {
+                    return {"type": "Feature","geometry": {"type": "Point","coordinates": [parseFloat(`${(x[5])}`), parseFloat(`${x[6]}`)]},"properties": {"name": `${x[2]}`}};   
+                } else {return 0} 
             });
         }
 
-    fetch('https://opensky-network.org/api/states/all')
+        // reference: https://gist.github.com/geog4046instructor/80ee78db60862ede74eacba220809b64
+        function createCustomIcon(Feature, latlng) {
+            var planeIcon = L.icon({
+                iconUrl: 'plane2.png',
+                iconSize:     [20, 20], 
+                iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location
+                popupAnchor:  [0, 0], // point from which the popup should open relative to the iconAnchor
+            })
+            return L.marker(latlng, {icon: planeIcon})
+        }
+
+        var getMarkerInfo = function(jsonInput) {
+            
+            return jsonInput.states.map(function(x) {
+                return `callsign : ${x[1]}` + "\n" + `icao24 : ${x[0]}`  + `longitude : ${x[5]}` + `latitude : ${x[6]}` + `velocity : ${x[9]}` + `altitude : ${x[13]}` + `heading  : ${x[10]}`
+
+            })
+        }
+
+
+    function update() {fetch('https://opensky-network.org/api/states/all')
         .then(function(response){
             return response.json();
         })
         .then(function(json){
+            setTimeout(update, 300000);// reference: https://stackoverflow.com/questions/38925157/proper-way-to-update-data-using-setinterval
             console.log(json)
             console.log(getLongitudeLatitudeName(json))
+            console.log(getMarkerInfo(json))
 
-            L.geoJSON(getLongitudeLatitudeName(json)).addTo(map);
+            var myLayerOptions = {
+                pointToLayer : createCustomIcon
+            }
 
+            // t.appendChild(i)
 
-
-
-
-
-
-
-
-
+            var z = document.createElement('p')
+            z.innerHTML = 'sldkjflskdjf'
 
 
+        L.geoJSON(getLongitudeLatitudeName(json), myLayerOptions).addTo(map).bindPopup(document.body.appendChild(z)).on(onclick);
 
 
-        }) // end of fetch 
+
+
+
+
+
+
+
+
+
+
+        }).catch(function(err){
+            setTimeout(update, 300000);
+        })// end of fetch 
+        }
+        update();
 
 })()
 
